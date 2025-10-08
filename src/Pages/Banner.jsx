f764c3e8f1f37c93a75fd9d546de4755d9c58059
Md. Saiful Islam <saiful.doict@gmail.com>
2025-10-08 10:24:17 +0600

    banner button link added

--- src/Pages/Banner.jsx
+++ src/Pages/Banner.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import hero from '../assets/hero.png'
 import playStore from '../assets/playStore.jpg'
 import appStore from '../assets/appStore.jpg'
+import { Link } from 'react-router'
 const Banner = () => {
   return (
     <div>
@@ -12,8 +13,8 @@ const Banner = () => {
                 <p className='text-gray-400 mt-4'>At HERO.IO, we craft innovative apps designed to make everyday life simpler, smarter, and more exciting.<br/>Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
 
                 <div className='flex justify-center gap-5 mt-11'>
-                    <button className='btn btn-outline outline-gray-200'><img src={playStore} className='w-6'/> Google Play</button>
-                    <button className='btn btn-outline outline-gray-200'><img src={appStore} className='w-6'/>App Store</button>
+                    <Link to='https://play.google.com/store/games?hl=en'><button className='btn btn-outline outline-gray-200'><img src={playStore} className='w-6'/> Google Play</button></Link>
+                    <Link to="https://www.apple.com/app-store/"><button className='btn btn-outline outline-gray-200'><img src={appStore} className='w-6'/>App Store</button></Link>
                 </div>
             </div>
             <img src={hero} className='mx-auto mt-11'/>
